7b69e9fd4760e14ecd58f13ec8728987a3d39e32
Marco Velásquez <velasquez.marco@outlook.com>
2018-12-10 14:45:58 -0400

    Add service REGISTER_MENU

--- app/src/main/java/com/example/markito/appproyecto/utils/Data.java
+++ app/src/main/java/com/example/markito/appproyecto/utils/Data.java
@@ -1,7 +1,8 @@
 package com.example.markito.appproyecto.utils;
 
 public class Data {
-    public static String TOKEN = "";
+    public static String TOKEN = "data [ponga aqui su token]";
     public static String HOST = "http://localhost:7777/api/v1.0";
     public static String SERVICIO = HOST + "";
+    public static String REGISTER_MENU = HOST + "/menu";
 }
