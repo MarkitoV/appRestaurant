f180a2bc25f48af4ea3ddf6d31cc572cae41774e
Marco Velásquez <velasquez.marco@outlook.com>
2018-12-09 21:59:50 -0400

    Add camera for menus

--- app/src/main/java/com/example/markito/appproyecto/MenuActivity.java
+++ app/src/main/java/com/example/markito/appproyecto/MenuActivity.java
@@ -1,13 +1,77 @@
 package com.example.markito.appproyecto;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MenuActivity extends AppCompatActivity {
+    private ImageButton btnPhoto;
+    private final int CODE = 100;
+    private final int CODE_PERMISSIONS = 101;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        btnPhoto = (ImageButton) findViewById(R.id.btnPhoto);
+
+        btnPhoto.setVisibility(View.INVISIBLE);
+        if (reviewPermissions()) {
+            btnPhoto.setVisibility(View.VISIBLE);
+        }
+
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                MenuActivity.this.startActivityForResult(camera, CODE);
+            }
+        });
+    }
+
+    private boolean reviewPermissions() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        if (this.checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        requestPermissions(new String[] {Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE}, CODE_PERMISSIONS);
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CODE_PERMISSIONS) {
+            if (permissions.length == 3) {
+                btnPhoto.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CODE) {
+            Bitmap img = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(img);
+        }
     }
 }
---
+++ app/src/main/java/com/example/markito/appproyecto/utils/Data.java
@@ -0,0 +1,7 @@
+package com.example.markito.appproyecto.utils;
+
+public class Data {
+    public static String TOKEN = "";
+    public static String HOST = "http://localhost:7777/api/v1.0";
+    public static String SERVICIO = HOST + "";
+}
